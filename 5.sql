cf7d37319c72d367657ff91b273ac98f6ec358f1
Rutvik <rsprutvik9970@gmail.com>
2023-03-15 23:18:13 +0530

    creating a table in the database

---
+++ 5.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Student(
+    ID INT PRIMARY KEY,
+    First_Name VARCHAR(20) NOT NULL,
+    Last_Name VARCHAR(20) NOT NULL,
+    City CHAR(20) NOT NULL,
+    Age INT NOT NULL,
+    Date_Of_Joining Date NOT NULL,
+    Result INT NOT NULL
+)
